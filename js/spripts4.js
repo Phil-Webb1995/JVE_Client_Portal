b816f1acf4529b35c1f10625148cd3862a155f66
Phil Webb <Philwebb19955@gmail.com>
2024-06-18 20:30:46 +0100

    First Commit - committing all files to repository

---
+++ js/spripts4.js
@@ -0,0 +1,23 @@
+console.log("scripts4.js loaded")
+
+var name = "Phil Webb"
+
+console.log(typeof full_name);
+
+var age = 30;
+console.log(`age needs to be a number a:`typeof age);
+
+if(age>=18){
+    console.log("You are an adult")
+}
+
+var persona = {
+    name: full_name,
+    age,
+};
+
+function eat() {
+    console.log("")
+}
+
+console.log(`persona`, persona);  
